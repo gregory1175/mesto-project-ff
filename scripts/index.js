b6144d90c1c8dcee2c6feefb5b5d372e8d8ffbb2
gregory1175 <151869985+gregory1175@users.noreply.github.com>
2024-03-06 02:38:59 +0300

    fixcallback

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,15 @@
 let cardList = document.querySelector('.places__list');
-let cardTemplate = document.querySelector('#card-template');
+const cardTemplate = document.querySelector('#card-template');
 
 const createCardElement = (cardInfo, removeCallBack) => {
 const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true);
 
 cardElement.querySelector('.card__image').src = cardInfo.link;
-cardElement.querySelector('.card__title').alt = cardInfo.name;
+cardElement.querySelector('.card__image').alt = cardInfo.name;
 cardElement.querySelector('.card__title').textContent = cardInfo.name;
 
 let deleteButton = cardElement.querySelector('.card__delete-button');
 deleteButton.addEventListener('click', () => {
-cardElement.remove();
 removeCallBack();
 
 });
@@ -24,5 +23,7 @@ cardElement.remove();
 
 initialCards.forEach((cardInfo) => {
 const cardElement = createCardElement(cardInfo, () => removeCard(cardElement));
+cardElement.remove();
+
 cardList.append(cardElement);
 });
